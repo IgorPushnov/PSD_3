3e45faa5d4362d057d5c18c2a2aa7cfc9e5e261e
IgorPushnov <Kompanella48@gmail.com>
2020-04-03 17:28:46 +0300

    changed files

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
-window.addEventListener('load', registerEvents, false);
-function registerEvents(e) {
-	document.getElementById("btn1").addEventListener('click', makeTiny, true);
-	document.getElementById("btn2").addEventListener('click', makeTiny2, true);
-}
-function makeTiny(e) {
-	document.getElementById("services__item").style.backgroundColor = "#00897b";
-	document.getElementById("services__title").style.color = "#fff";
-	document.getElementById("services__subtitle").style.color = "#fff";
-	document.getElementById("btn1").style.backgroundColor = "#00897b";
-	document.getElementById("btn1").style.color = "#fff";
-	document.getElementById("btn1").style.borderColor = "#fff";
-}
+// window.addEventListener('load', registerEvents, false);
+// let elem1 = document.getElementById("btn1");
+// let elem2 = document.getElementById("btn2");
 
-function makeTiny2(e) {
-	document.getElementById("services__item2").style.backgroundColor = "#00897b";
-	document.getElementById("services__title2").style.color = "#fff";
-	document.getElementById("services__subtitle2").style.color = "#fff";
-	document.getElementById("btn2").style.backgroundColor = "#00897b";
-	document.getElementById("btn2").style.color = "#fff";
-	document.getElementById("btn2").style.borderColor = "#fff";
-}
+
+// function registerEvents(ret) {
+// 	elem1.addEventListener('onfocus', makeTiny1, true);
+// 	elem2.addEventListener('onfocus', makeTiny2, true);
+// }
+
+// function makeTiny1(e) {
+// 	makeRiny1(e);
+// 	document.getElementById("services__item1").style.backgroundColor = "#00897b";
+// 	document.getElementById("services__title1").style.color = "#fff";
+// 	document.getElementById("services__subtitle1").style.color = "#fff";
+// 	document.getElementById("services__icon1").style.backgroundImage = "url(../img/screen/005.png)";
+// }
+
+// function makeTiny2(e) {
+// 	makeRiny2(e);
+// 	document.getElementById("services__item2").style.backgroundColor = "#00897b";
+// 	document.getElementById("services__title2").style.color = "#fff";
+// 	document.getElementById("services__subtitle2").style.color = "#fff";
+// 	document.getElementById("services__icon2").style.backgroundImage = "url(../img/screen/005.png)";
+// }
+
+// function makeRiny1(e) {
+// 	document.getElementById("services__item2").style.backgroundColor = "#fff";
+// 	document.getElementById("services__title2").style.color = "#000";
+// 	document.getElementById("services__subtitle2").style.color = "#000";
+// 	document.getElementById("services__icon2").style.backgroundImage = "url(../img/screen/004.png)";
+// }
+
+// function makeRiny2(e) {
+// 	document.getElementById("services__item1").style.backgroundColor = "#fff";
+// 	document.getElementById("services__title1").style.color = "#000";
+// 	document.getElementById("services__subtitle1").style.color = "#000";
+// 	document.getElementById("services__icon1").style.backgroundImage = "url(../img/screen/004.png)";
+// }
+
